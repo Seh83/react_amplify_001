8b8876c10f62b66f6ba81bca5ac58372cd1c61d9
Sehul Viras <sehul.viras@live.com>
2023-05-20 22:35:06 -0500

    Updating the boilder plate code to test changes!

--- src/App.js
+++ src/App.js
@@ -10,16 +10,13 @@ function App() {
           className="App-logo"
           alt="logo"
         />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in a day!
+          AWS Amplify + ReactJs Practice #001
         </a>
       </header>
     </div>
