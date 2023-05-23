9d3df2fa8aa807f2c52b43e9193b62e1bab3cf26
Sehul Viras <sehul.viras@live.com>
2023-05-23 00:00:50 -0500

    Added APIs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,66 @@
-import React from 'react';
-import menuLinksData from './data/menu_links.json'
+import React, { useState, useEffect } from "react";
+//import menuLinksData from './data/menu_links.json'
 
 const Header = () => {
+  const [menuLinksData, setMenuLinksData] = useState([]);
+
+  const loadMenuLinksData = async () => {
+    try {
+      const resp = await fetch(
+        "https://0ap34ldvw1.execute-api.us-east-1.amazonaws.com/Production/menulinks"
+      );
+      if (!resp.ok) {
+        throw new Error(`HTTP error! Status: ${resp.status}`);
+      }
+      let jsonData = await resp.json();
+      setMenuLinksData(jsonData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //Side Effects
+  useEffect(() => {
+    loadMenuLinksData();
+  }, []);
+
   return (
     <header id="intro">
       <article className="fullheight">
         <div className="hgroup">
           <h1>Landon Hotel</h1>
           <h2>West London</h2>
-          <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a></p>
+          <p>
+            <a href="#welcome">
+              <img
+                src="https://landonhotel.com/images/misc/arrow.png"
+                alt="down arrow"
+              />
+            </a>
+          </p>
         </div>
       </article>
 
       <nav id="nav">
         <div className="navbar">
-          <div className="brand"><a href="#welcome">Landon <span>Hotel</span></a></div>
+          <div className="brand">
+            <a href="#welcome">
+              Landon <span>Hotel</span>
+            </a>
+          </div>
           <ul>
-            {
-              menuLinksData.map((link) =>
-                <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
-              )
-            }
+            {menuLinksData.map((link) => (
+              <li>
+                <a className={`icon ${link.class}`} href={link.href}>
+                  <span>{link.text}</span>
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,22 +1,53 @@
-import React from 'react';
-import galleryImagesData from './data/gallery_images.json'
+import React, { useState, useEffect } from "react";
+//import galleryImagesData from "./data/gallery_images.json";
 
 const Welcome = () => {
+  const [galleryImagesData, setGalleryImagesData] = useState([]);
+
+  const loadGalleryImagesData = async () => {
+    try {
+      const resp = await fetch(
+        "https://0ap34ldvw1.execute-api.us-east-1.amazonaws.com/Production/galleryimages"
+      );
+      if (!resp.ok) {
+        throw new Error(`HTTP error! Status: ${resp.status}`);
+      }
+      let jsonData = await resp.json();
+      setGalleryImagesData(jsonData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //Side Effects
+  useEffect(() => {
+    loadGalleryImagesData();
+  }, []);
+
   return (
     <div className="scene" id="welcome">
       <article className="content">
         <div className="gallery">
-          {
-            galleryImagesData.map((image) =>
-              <img className={image.className} src={image.src} alt={image.alt} />
-            )
-          }
+          {galleryImagesData.map((image) => (
+            <img className={image.className} src={image.src} alt={image.alt} />
+          ))}
         </div>
         <h1>Welcome to the Landon&nbsp;Hotel</h1>
-        <p>The original Landon perseveres after 50 years in the heart of West London. The West End neighborhood has something for everyone—from theater to dining to historic sights. And the not-to-miss Rooftop Cafe is a great place for travelers and locals to engage over drinks, food, and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel in the West End, browse our website and <a href="files/landon_information_sheet_London.pdf">download our handy information sheet</a>.</p>
+        <p>
+          The original Landon perseveres after 50 years in the heart of West
+          London. The West End neighborhood has something for everyone—from
+          theater to dining to historic sights. And the not-to-miss Rooftop Cafe
+          is a great place for travelers and locals to engage over drinks, food,
+          and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel
+          in the West End, browse our website and{" "}
+          <a href="files/landon_information_sheet_London.pdf">
+            download our handy information sheet
+          </a>
+          .
+        </p>
       </article>
     </div>
   );
-}
+};
 
 export default Welcome;
